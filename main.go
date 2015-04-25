46369cde6d4ec12f3e5a310310ec1e4d4c6d353a
Watrelos Victor <vwatrelos@kilix.fr>
2015-04-25 16:30:07 +0200

    Add mongodb open, insert, close

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"./bdd"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("HELLO")
+	db := bdd.BddManager{}
+	db.GetConnection()
+	data := bdd.MusicData{"NOFX"}
+	db.InsertData(&data)
+	db.Close()
 }
