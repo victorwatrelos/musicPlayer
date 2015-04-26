c9028b2cb2191fcd75ae70a378035a41cb7f556c
Joseph Renouf-Gay <renoufjoseph@gmail.com>
2015-04-26 14:21:47 +0200

    link crawl db

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"./crawl"
 	"./db"
+	"flag"
 	"time"
 )
 
 func main() {
+	flag.Parse()
 	db := db.DBManager{}
 	db.GetConnection()
 	c := make(chan crawl.Music)
 	crawler := crawl.Crawler{c}
 	go db.ReadMusicChan(c)
-	crawler.Go(".")
+	//	crawler.Go(".")
+	root := flag.Arg(0)
+	crawler.Go(root)
 	time.Sleep(10000)
 	//	db.Query("artist", "O")
 	db.Close()
