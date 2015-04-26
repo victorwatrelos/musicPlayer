9dae09e92d18681ff5aa68733493f4b8594bdc65
Watrelos Victor <vwatrelos@kilix.fr>
2015-04-26 18:52:58 +0200

    FIX

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"./db"
 	"./player"
 	"flag"
+	"time"
 )
 
 func main() {
-	bdd := db.DBManager{}
-	bdd.Query("artist", "NOFX")
 	flag.Parse()
 	play := player.Player{}
-	play.Add(flag.Arg(0))
-	play.Play()
 }
--- player/Player.go
+++ player/Player.go
@@ -4,38 +4,56 @@ import (
 	"fmt"
 	"io"
 	"os/exec"
+	"time"
 )
 
 type Player struct {
-	input io.WriteCloser
-	cmd   *exec.Cmd
+	input  io.WriteCloser
+	output io.ReadCloser
+	cmd    *exec.Cmd
 }
 
 func (p *Player) Init() {
 	fmt.Println("Starting vlc")
 	var err error
-	p.cmd = exec.Command("vlc", "-I", "rc")
-	p.cmd.Start()
+	p.cmd = exec.Command("/Applications/VLC.app/Contents/MacOS/VLC", "-I", "rc")
 	p.input, err = p.cmd.StdinPipe()
+	p.output, err = p.cmd.StdoutPipe()
 	chk(err)
+	errr := p.cmd.Start()
+	chk(errr)
+	time.Sleep(100)
+}
+
+func (p *Player) RunCmd(cmd string) {
+	println("Cmd: ", cmd)
+	_, err := p.input.Write([]byte(cmd))
+	chk(err)
+}
+
+func (p *Player) Pause() {
+	p.RunCommand("pause\n")
 }
 
 func (p *Player) Play() {
+	p.RunCommand("play\n")
+}
 
-	_, err := p.input.Write([]byte("play\n"))
-	chk(err)
+func (p *Player) SetVolume(vol int) {
+	if vol > 1024 {
+		vol = 1024
+	}
+	if vol < 0 {
+		vol = 0
+	}
 }
 
 func (p *Player) Add(filename string) {
-	cmd := "add ../music.mp3\n"
-	cmd2 := []byte(cmd)
-	fmt.Println(cmd2)
-	fmt.Println(len(cmd2))
-	_, err := p.input.Write(cmd2)
-	chk(err)
+	p.RunCmd("add " + filename + "\n")
 }
 
 func (p *Player) Close() {
+	println("Close")
 	p.input.Close()
 }
 
